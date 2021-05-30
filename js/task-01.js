2549c5a6fc8738f8a7eda2658a85837b2b619feb
kovalchuktaisiia <kovalchuk.taisiia@google.com>
2021-05-30 17:57:53 +0300

    first

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
 
 const delay = ms => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
-        resolve({ms});
+        resolve(ms);
         }, ms )
       });
   };
